1010d232ed10f43d8d6e5356d857b164a4bddc36
Sam <s.goldie@student.maastrichtuniversity.nl>
2023-03-16 15:23:25 +0100

    added Runner

---
+++ src/main/java/Run.java
@@ -0,0 +1,8 @@
+import gui.ChatApplication;
+import javafx.application.Application;
+
+public class Run {
+    public static void main(String[] args) {
+        Application.launch(ChatApplication.class, args);
+    }
+}
