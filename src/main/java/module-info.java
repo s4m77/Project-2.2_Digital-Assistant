d6c0e7acebbc07414dff28738ceddfb89a982086
hypster1995 <fraizalee1978@gmail.com>
2023-03-16 01:06:50 +0100

    fixed json error Now WikipediaAPI working

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,13 @@ module gui {
     requires java.sql;
     requires org.junit.jupiter.api;
     requires org.jsoup;
+    requires org.apache.httpcomponents.httpcore;
+    requires org.apache.httpcomponents.httpclient;
+    requires org.json;
 
 
     opens gui to javafx.fxml;
     exports gui;
+
+
 }
