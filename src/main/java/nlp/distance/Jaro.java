9ed3ddd25e58e395836c93eebc92036971f63aa8
hypster1995 <fraizalee1978@gmail.com>
2023-05-14 22:23:01 +0200

    jaro and qwerty distance

---
+++ src/main/java/nlp/distance/Jaro.java
@@ -0,0 +1,51 @@
+package nlp.distance;
+
+public class Jaro {
+    public static double calculate(String str1, String str2) {
+        int l = str1.length();
+        int m = str2.length();
+
+        if (l == 0 && m == 0) return 1;
+
+        int match_distance = Integer.max(l, m) / 2 - 1;
+
+        boolean[] s_matches = new boolean[l];
+        boolean[] t_matches = new boolean[m];
+
+        int matches = 0;
+        int transpositions = 0;
+
+        for (int i = 0; i < l; i++) {
+            int start = Integer.max(0, i-match_distance);
+            int end = Integer.min(i+match_distance+1, m);
+
+            for (int j = start; j < end; j++) {
+                if (t_matches[j]) continue;
+                if (str1.charAt(i) != str2.charAt(j)) continue;
+                s_matches[i] = true;
+                t_matches[j] = true;
+                matches++;
+                break;
+            }
+        }
+
+        if (matches == 0) return 0;
+
+        int k = 0;
+        for (int i = 0; i < l; i++) {
+            if (!s_matches[i]) continue;
+            while (!t_matches[k]) k++;
+            if (str1.charAt(i) != str2.charAt(k)) transpositions++;
+            k++;
+        }
+
+        return (((double)matches / l) +
+                ((double)matches / m) +
+                (((double)matches - transpositions/2.0) / matches)) / 3.0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculate("hello", "hel"));
+
+    }
+}
---
+++ src/main/java/nlp/distance/Qwerty.java
@@ -0,0 +1,64 @@
+package nlp.distance;
+
+public class Qwerty {
+
+    public static void main(String[] args){
+
+        String s1 = "hello";
+        String s2 = "helmo";
+        int qwertyDistance = Qwerty.qwerty(s1, s2);
+        System.out.println(qwertyDistance);
+
+    }
+
+    // QWERTY keyboard layout
+    private static final String[] KEYBOARD = {
+            "`1234567890-=",
+            " qwertyuiop[]\\",
+            " asdfghjkl;'",
+            " zxcvbnm,./"
+    };
+
+    // Calculate the distance between two characters on the keyboard
+    private static int distance(char c1, char c2) {
+        int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
+        for (int i = 0; i < KEYBOARD.length; i++) {
+            int index = KEYBOARD[i].indexOf(c1);
+            if (index != -1) {
+                x1 = index;
+                y1 = i;
+            }
+            index = KEYBOARD[i].indexOf(c2);
+            if (index != -1) {
+                x2 = index;
+                y2 = i;
+            }
+        }
+        if (x1 == -1 || x2 == -1) {
+            return Integer.MAX_VALUE;
+        }
+        return (int) Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    }
+
+    //  Calculate the QWERTY distance between two strings
+    public static int qwerty(String str1, String str2) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int[][] dis = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) {
+            dis[i][0] = i;
+        }
+        for (int j = 0; j <= len2; j++) {
+            dis[0][j] = j;
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                int cost = distance(str1.charAt(i - 1), str2.charAt(j - 1));
+                dis[i][j] = Math.min(dis[i - 1][j - 1] + cost, Math.min(dis[i - 1][j] + 1, dis[i][j - 1] + 1));
+            }
+        }
+        return dis[len1][len2];
+    }
+
+}
+
