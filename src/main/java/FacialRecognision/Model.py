79fcd1521d9ceee5477a8e45f2b6bd012f955bf3
Tom Verbeek <58432711+Johnicusswagicus@users.noreply.github.com>
2023-06-16 11:14:01 +0200

    allow string method ading

--- src/main/java/FacialRecognision/Model.py
+++ src/main/java/FacialRecognision/Model.py
@@ -4,14 +4,23 @@ import cv2
 import numpy as np
 import os
 import joblib
+import matplotlib.pyplot as plt
 ##highgui is used to show the image
 
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from skimage.feature import local_binary_pattern
+from sklearn.model_selection import train_test_split
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 class Model:
     MODEL_PATH = "model.joblib"
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     model=None
+    picturePath="src\\main\\resources\\Faces"
+    Certainity=0.6
+
     def __init__(self):
         if os.path.exists(self.MODEL_PATH):
             self.model = joblib.load(self.MODEL_PATH)
@@ -42,9 +51,63 @@ class Model:
         for i in gray:
             predictions.append(self.model.predict(i.reshape(1,-1)))
         return predictions
+    
+
     def findFaces(self,image):
         faces=self.face_cascade.detectMultiScale(image, 1.3, 5)
         return faces
+    
+
+    def extract_features(self,image):
+        ##method assumes that the image is a grayscale image
+
+        # Apply LBP
+        radius = 1
+        num_points = 8 * radius
+        lbp = local_binary_pattern(image, num_points, radius, method='uniform')
+        
+        # Flatten the LBP image into a feature vector
+        feature_vector = lbp.flatten()
+        
+        return feature_vector
+    
+    def train(self,components=100):
+        dir=self.picturePath
+        Labels=os.listdir(dir)
+
+
+        temp2=[dir+"/"+i for i in Labels]
+        images=np.array([plt.imread(image) for image in temp2])
+        Labels=np.array(Labels)
+        ##convert to grayscale
+        proccessed= [self.process(i) for i in images]
+        
+        Vectors=np.array([self.extract_features(i) for i in proccessed])
+        normalized=(Vectors-np.mean(Vectors))/np.std(Vectors)
+        pca=PCA(n_components=components)
+        reduced=pca.fit_transform(normalized)
+        classifier=KNeighborsClassifier(n_neighbors=1)
+        classifier.fit(reduced,Labels)
+        joblib.dump(classifier,self.MODEL_PATH)
+        print("Model trained")
+        return classifier
+    
+    def getCertainity(self,image,label):
+        ##use cosine similarity to get the certainity of the prediction
+        gray=self.process(image)
+        feature_vector=self.extract_features(gray)
+        normalized=(feature_vector-np.mean(feature_vector))/np.std(feature_vector)
+        pca=PCA(n_components=100)
+        reduced=pca.fit_transform(normalized)
+        prediction=self.model.predict(reduced)
+        certainity=cosine_similarity(reduced,self.model.embedding_)
+        return certainity
+
+
+
+    def isFace(self,image,label):
+        certainity=self.getCertainity(image,label)
+        return certainity>self.Certainity
 
 
 
